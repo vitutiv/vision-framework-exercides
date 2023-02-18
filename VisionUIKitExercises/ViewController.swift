fcdafb60a457e6ff2b9f9823563f530910a5fd7f
Victor Pereira Moura <vitutivitu@gmail.com>
2023-02-18 11:31:57 -0300

    Initial Commit

---
+++ VisionUIKitExercises/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VisionUIKitExercises
+//
+//  Created by Victor on 18/02/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
